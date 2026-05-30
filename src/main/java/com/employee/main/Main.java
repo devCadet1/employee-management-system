504b9b6bc80c0386ce0330301e6de016c99e8053
devCadet <satyam.karn3629@gmail.com>
2026-05-30 21:52:23 +0530

    Main class

--- src/main/java/com/employee/main/Main.java
+++ src/main/java/com/employee/main/Main.java
@@ -1,7 +1,33 @@
 package com.employee.main;
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
 
 public class Main {
 	public static void main (String[] args) {
-		System.out.println("Employee Management System");
+		
+		Employee emp1 = new Employee(101, "satyam", "SDE" , 100000);
+		Employee emp2 = new Employee(102, "logesh", "Game Programmer", 100000);
+		Employee emp3 = new Employee(103, "nikhil", "Game Programmer", 100000);
+		Employee emp4 = new Employee(104, "harsh", "HR", 40000);
+		Employee emp5 = new Employee(105, "mansi", "TL", 120000);
+		
+		EmployeeRepository repository = new EmployeeRepository();
+		
+		repository.addEmployees(emp1);
+		repository.addEmployees(emp2);
+		repository.addEmployees(emp3);
+		repository.addEmployees(emp4);
+		repository.addEmployees(emp5);
+		
+//		System.out.println(repository.getAllEmployees());
+		
+		Employee employee = repository.findEmployeeById(101);
+		System.out.println(employee);
+		
+		Employee emp = repository.findEmployeeByName("mansi");
+		System.out.println(emp);
+		
+		
+		
 	}
 }
